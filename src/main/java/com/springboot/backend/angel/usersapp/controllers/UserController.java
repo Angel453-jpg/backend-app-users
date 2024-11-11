5909a9da59e1b01d88bda12385c7b081770e64e9
Angel <angelin09ozoz@gmail.com>
2024-11-11 09:35:59 -0600

    Implementando el controladorRestController métodos Handler

--- src/main/java/com/springboot/backend/angel/usersapp/controllers/UserController.java
+++ src/main/java/com/springboot/backend/angel/usersapp/controllers/UserController.java
@@ -1,7 +1,40 @@
 package com.springboot.backend.angel.usersapp.controllers;
 
+import com.springboot.backend.angel.usersapp.entities.User;
+import com.springboot.backend.angel.usersapp.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> list() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> show(@PathVariable Long id) {
+
+        Optional<User> userOptional = userService.findById(id);
+        if (userOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(userOptional.orElseThrow());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("error", "El usuario no se encontró por el id: " + id));
+
+    }
+
 }
