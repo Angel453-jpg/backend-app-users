6c4f0fee6dea85f6da10dcacc181771a8206559b
Angel <angelin09ozoz@gmail.com>
2024-11-13 23:23:55 -0600

    Validando el controller en los métodos POST y PUT

--- src/main/java/com/springboot/backend/angel/usersapp/controllers/UserController.java
+++ src/main/java/com/springboot/backend/angel/usersapp/controllers/UserController.java
@@ -2,13 +2,13 @@ package com.springboot.backend.angel.usersapp.controllers;
 
 import com.springboot.backend.angel.usersapp.entities.User;
 import com.springboot.backend.angel.usersapp.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -38,12 +38,21 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody User user) {
+    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return validation(result);
+        }
+
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+    public ResponseEntity<?> update(@Valid @RequestBody User user, BindingResult result, @PathVariable Long id) {
+
+        if (result.hasErrors()) {
+            return validation(result);
+        }
 
         Optional<User> userOptional = userService.findById(id);
         if (userOptional.isPresent()) {
@@ -72,4 +81,12 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("error", "El usuario no existe"));
     }
 
+    private static ResponseEntity<Map<String, String>> validation(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+        result.getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), "El campo " + error.getField() + " " + error.getDefaultMessage());
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
